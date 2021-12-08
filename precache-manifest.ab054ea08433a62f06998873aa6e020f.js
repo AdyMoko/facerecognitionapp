58a13a762a7b2be3ba01d3065d8959d8c196cc59
AdyMoko <mocodean_adrian@yahoo.com>
2021-12-08 20:07:20 +0200

    Updates

---
+++ precache-manifest.ab054ea08433a62f06998873aa6e020f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf95409ffd5221bac4909e75b3a63a7",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "758980afce3f75108009",
+    "url": "/facerecognitionapp/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "22bfe181e472588ff882",
+    "url": "/facerecognitionapp/static/css/main.69353b7f.chunk.css"
+  },
+  {
+    "revision": "758980afce3f75108009",
+    "url": "/facerecognitionapp/static/js/2.b1b4e44d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/facerecognitionapp/static/js/2.b1b4e44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e368526f97445419e0",
+    "url": "/facerecognitionapp/static/js/3.de3ec778.chunk.js"
+  },
+  {
+    "revision": "22bfe181e472588ff882",
+    "url": "/facerecognitionapp/static/js/main.ee17e2e8.chunk.js"
+  },
+  {
+    "revision": "55ac83347952bb2ec282",
+    "url": "/facerecognitionapp/static/js/runtime-main.4b079ff9.js"
+  },
+  {
+    "revision": "95eab33342edbe8f76945426e168d2f5",
+    "url": "/facerecognitionapp/static/media/brain1.95eab333.png"
+  }
+]);
